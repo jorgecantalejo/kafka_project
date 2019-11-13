1c9c7da9e96abe426f38a3c6832e73498ffcf8eb
jorge <jcantalejo@virtualdesk.es>
2019-11-13 15:43:16 +0100

    Servicio productor de mensajes

---
+++ src/main/java/com/kafka/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.kafka.controller;
+
+import com.kafka.productor.MessageProducer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+	@Autowired
+	private MessageProducer messageProducer;
+
+	@PostMapping("/users")
+	public String sendMessage(@RequestParam("message") String message, @RequestParam("topic") String topic) {
+		messageProducer.sendMessage(message, topic);
+		return "Mensaje enviado";
+	}
+}
